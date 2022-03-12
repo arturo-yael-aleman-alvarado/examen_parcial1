4ceb5b43d17bfbfb01b43132ddd95189e45402a9
arturo-yael-aleman-alvarado <franmg555@gmail.com>
2022-03-12 15:54:42 -0600

    Examen Resuelto Bien

---
+++ src/uaslp/objetos/parcial1/examen/pos/CartItem.java
@@ -0,0 +1,30 @@
+package uaslp.objetos.parcial1.examen.pos;
+
+public class CartItem {
+    private int quantity;
+    private Product product;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getProductName() {
+        return product.getName();
+    }
+
+    public float getAmount(){
+        return product.getAmount();
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
